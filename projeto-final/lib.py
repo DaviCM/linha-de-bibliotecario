74c05bdca20d8f7ecede710a94b145a4eb8737ad
DaviCM <davimaciel2008@gmail.com>
2025-09-18 11:42:48 -0300

    .

--- projeto-final/lib.py
+++ projeto-final/lib.py
@@ -2,6 +2,8 @@ from time import sleep
 from os import system
 import json
 
+# Função básica, que utiliza os comandos de CLI para limpar a tela.
+# Utilizado para gegar erro genérico em muitos lugares do código.
 def continuar():
     system('cls')
     print('Opção inválida. Tente novamente.')
@@ -9,28 +11,34 @@ def continuar():
     system('cls')
 
 
-def pegarJSON():
+# Função que abre o JSON em uma variável e retorna ela, junto com uma lista que contém os nomes dos livros
+def pegarJSON(): 
     try:
         livros = []
-        
+        # Adicionar o arquivo JSON à uma lista 'data', que contém todos os dicts
         with open('projeto-final/db.json', 'r', encoding='utf-8') as file:
             data = (json.load(file))
         
-        # Armazena todas as keys (nomes dos livros) em um dict, para acesso mais fácil nas outras funções
+        # Armazena todas as keys (nomes dos livros) em uma lista 'livros', para acesso mais fácil nas outras funções
+        # Primiero loop: itera sobre cada dicionário da lista de dicts
+        # Segundo loop: Dentro desse dicionário, irá iterar sobre seu índice e o armazenar na lista 'livros'.
         for dicts in data:
             for key in dicts:
                 livros.append(key)
         
+        # As outras funções recebem essa tupla para manipular os dados, sempre no início dos loops de execução delas.
+        # E desemapcotam a tupla em vars
         return data, livros
     
     except json.decoder.JSONDecodeError:
-        # Retorna as duas listas vazias, correspondentes a lista de dicionários e a lista de chaves.
-        # Forma mais fácil de tratar do que retornando False, pois permite a criação fácil de novas categorias no JSON
-        # Permite um menu diferete em 'modificar' caso a lista data esteja vazia
-        # E permite que o tratamento seja mais conciso.
+        # Retorna as duas listas vazias, correspondentes às listas de dicionários e de lista de chaves.
+        # Forma mais fácil de tratar do que retornando False, pois 'data' nas outras funções recebe []
+        # Logo, os livros serão adicionados a ela normalmente.
         return [], []
 
 
+# Recebe uma lista 'data' e adiciona ela ao JSON com W, reescrevendo toda a DB.
+# Melhor que append, pois ele adiciona uma outra lista e altera a indentação.
 def adicionarAoJSON(data):
     with open('projeto-final/db.json', 'w', encoding='utf-8') as file:
         # Data sempre será uma lista, logo ele irá adicionar os valores à lista mesmo que ela se inicializar vazia
@@ -38,31 +46,44 @@ def adicionarAoJSON(data):
         json.dump(data, file, ensure_ascii=False, indent=4)
 
 
-def pegarNome():
-    nome = (input('Digite o título do livro: ').title()).strip()
-    return nome
-
-
-def pegarAutores():
-    autores = []
-    c = 1
-    
+# retorna uma string contendo apenas caracteres alfanuméricos, se o check der certo.
+# Usada para pegar nome e gênero do livro.
+def pegarSTR(inputValue):
     while True:
-        system('cls')
-        autor = (input(f'Digite o nome do {c}° autor do livro (enter para parar): ').title()).strip()
-        if autor != '':
-            autores.append(autor)
-            c += 1
-            continue
-        elif (autor == '') and (autores != []):
-            system('cls')
-            print('Todos os autores foram cadastrados.')
-            return autores
+        string = (input(inputValue).title()).strip()
+        if (''.join(string)).isalpha() == True:
+            return string
         else:
             continuar()
             continue
 
 
+# Lógica para armazenar múltiplos autores em uma lista com visualização agradável para o usuário
+def pegarAutores():
+    autores = []
+    c = 1 # Contador
+    
+    while True:
+        system('cls')
+        autor = (input(f'Digite o nome do {c}° autor do livro (enter para parar): ').title()).strip()
+        # Se for diferente de enter, irá adicionar o valor à lista e seguir para a próxima iteração.
+        if autor != '':
+            autores.append(autor)
+            c += 1
+            continue
+        # Se autor == enter e a lista estiver com algom valor, finaliza a execução e retorna.
+        elif (autor == '') and (autores != []):
+            system('cls')
+            print('Todos os autores foram cadastrados.')
+            return autores
+        # Se autor == enter e a lista estiver vazia, apenas envia a mensagem padrão de erro.
+        else:
+            continuar()
+            continue
+
+
+# Função que cadastra um livro, com estrutura de dict.
+# Parâmetro de retorno defaulta para False, já que só preciso dele uma vez (editar())
 def cadastrar(retornar=False):
     data, livros = pegarJSON()
     autores = []
@@ -73,7 +94,9 @@ def cadastrar(retornar=False):
 
         while True:
             system('cls')
-            nome = pegarNome()
+            nome = pegarSTR('Digite o título do livro: ')
+            # Se o nome inserido já estiver na lista, quebra
+            # Só pode haver um livro com cada nome.
             if nome in livros:
                 print('Esse nome já foi usado em outro livro. Insira outro.')
                 sleep(1)
@@ -84,7 +107,9 @@ def cadastrar(retornar=False):
         autores = pegarAutores()
         
         system('cls')
-        genero = (input('Digite o gênero do livro: ').title()).strip()
+        genero = pegarSTR('Digite o gênero do livro: ')
+
+        # Objeto dicionário que contém as informações obtidas do livro
         novoLivro = {nome : [autores, genero]}
         break
 
@@ -93,20 +118,24 @@ def cadastrar(retornar=False):
     sleep(1)
     system('cls')
 
+    # Se o valor de retorno for falso,irá seguir o normal e adicionar o livro ao JSON
     if retornar == False:
         data.append(novoLivro)
         adicionarAoJSON(data)
-    
+    # Se for verdadeiro, apenas retornará o novo livro
+    # função editar() precisa d valor de retorno, para substituir na posição específica.
     else:
         return novoLivro
 
 
+# Função que edita livros, apenas utilizando as funções anteriores
 def editar():
     while True:
 
         data, livros = pegarJSON()
         print('Editor de livros cadastrados''\n')
 
+        # Mostra as opções ao usuário imprimindo cada posição da lista de nomes
         for i in range(len(livros)):
             print(f'{i + 1} - (Livro) {livros[i]}')
         print(f'{i + 2} - Voltar ao menu principal''\n')
@@ -123,7 +152,8 @@ def editar():
         except ValueError:
             continuar()
             continue
-
+        
+        # Se a escolha for bem sucedida
         if opt < (i + 2):
             while True:
                 system('cls')
@@ -139,24 +169,20 @@ def editar():
                         data[opt - 1].get(livros[opt - 1])[0] = pegarAutores()
                         adicionarAoJSON(data)
                         break
-
                     case '2':
                         system('cls')
-                        data[opt - 1].get(livros[opt - 1])[1] = (input('Digite o gênero do livro: ').title()).strip()
+                        data[opt - 1].get(livros[opt - 1])[1] = pegarSTR('Digite o gênero do livro: ')
                         adicionarAoJSON(data)
                         break
-
                     case '3':
                         data[opt - 1] = cadastrar(retornar=True)
                         adicionarAoJSON(data)
                         sleep(0.5)
                         break
-
                     case '4':
                         print('Retornando ao menu de seleção de livros.')
                         sleep(0.5)
                         break
-
                     case _:
                         continuar()
                         continue
@@ -213,13 +239,11 @@ def remover():
                         print('Livro deletado.')
                         adicionarAoJSON(data)
                         break
-
                     case 'n':
                         system('cls')
                         print('Retornando ao menu de deleção.')
                         sleep(0.5)
                         break
-
                     case _:
                         continuar()
                         continue
@@ -230,69 +254,75 @@ def remover():
             break
 
 
+# Função de execução, gera o menu inicial e redireciona o usuário para a função desejada
+# A depender do input
 def main():
     while True:
         data, livros = pegarJSON()
 
         system('cls')
-        print('Bem-vindo ao gerenciador de livros da biblioteca!''\n')
-        print('1 - Visualizar livros cadastrados')
-        print('2 - Cadastrar novo livro')
-        print('3 - Editar informações de um livro')
-        print('4 - Remover livro')
-        print('5 - Sair')
-        opt = input('\n''Escolha o que deseja fazer: ').strip()
 
-        match opt:
-            case '1':
+        # Check para ver se a lista está vazia
+        # Caso estiver, user pode apenas cadastrar um livro ou sair
+        if livros == []:
+            opt = (input('A lista de livros está vazia. Deseja cadastrar um livro? (s/n): ').lower()).strip()
+            if opt == 's':
                 system('cls')
-                if livros != []:
+                cadastrar()
+                continue
+            elif opt == 'n':
+                system('cls')
+                print('Adeus, amigo.')
+                break
+            else:
+                continuar()
+                continue
+        else:
+            system('cls')
+            print('Bem-vindo ao gerenciador de livros da biblioteca!''\n')
+            print('1 - Visualizar livros cadastrados')
+            print('2 - Cadastrar novo livro')
+            print('3 - Editar informações de um livro')
+            print('4 - Remover livro')
+            print('5 - Sair')
+            opt = input('\n''Escolha o que deseja fazer: ').strip()
+
+            match opt:
+                case '1':
+                    system('cls')
+
+                    # Nested for loop, o primeiro itera sobre cada um dos dicts (livros) na lista
                     for livro in data:
+                        # O segundo itera sobre as keys deles, e gera a visualização com os nome (key), autor (get() na posição 0)
+                        # e gênero (get() na posição 1) para cada um dos livros.
                         for nome in livro:
                             print(f'Livro: {nome}')
                             print(f'Autor(es): {livro.get(nome)[0]}')
                             print(f'Gênero: {livro.get(nome)[1]}''\n')
-                    
+
                     sleep(5)
                     continue
-                # Crime
-                else:
-                    print('Não há livros cadastrados. Cadastre um livro antes de prosseguir.')
-                    sleep(0.5)
+
+                # A partir daqui, chama a função certa para cada opção do user.
+                case '2':
+                    system('cls')
+                    cadastrar()
                     continue
-
-            case '2':
-                system('cls')
-                cadastrar()
-
-            case '3':
-                system('cls')
-                if livros != []:
+                case '3':
+                    system('cls')
                     editar()
-                # Crime
-                else:
-                    print('Não há livros cadastrados. Cadastre um livro antes de prosseguir.')
-                    sleep(0.5)
                     continue
-
-            case '4':
-                system('cls')
-                if livros != []:
+                case '4':
+                    system('cls')
                     remover()
-                # Crime
-                else:
-                    print('Não há livros cadastrados. Cadastre um livro antes de prosseguir.')
-                    sleep(0.5)
                     continue
-
-            case '5':
-                system('cls')
-                print('Adeus, amigo.')
-                break
-
-            case _:
-                continuar()
-                continue
+                case '5':
+                    system('cls')
+                    print('Adeus, amigo.')
+                    break
+                case _:
+                    continuar()
+                    continue
 
 
 main()
