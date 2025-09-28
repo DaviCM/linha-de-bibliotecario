46c32b60aff41e3e8fb4d6b6b16103f373f950f4
DaviCM <davimaciel2008@gmail.com>
2025-09-28 15:45:04 -0300

    .

--- orientado-a-objeto/classes.py
+++ orientado-a-objeto/classes.py
@@ -1,88 +1,190 @@
-from time import sleep
-from os import system
+from os import getcwd
 import json
 
-def continuar():
-        system('cls')
-        print('Opção inválida. Tente novamente.')
-        sleep(0.5)
-        system('cls')
+# Classe - descrição de um 'formato geral' que os objetos devem ter
+# Atributos - as variáveis da classe, que são únicas para cada instância dela (características do objeto)
+# Métodos - Funções, as ações que os objetos podem realizar
 
+# self - 'nome da classe', porém referido apenas dentro dela mesma - usado para referenciar atributos de dentro.
+# construtor - necessário para acessar os atributos da classe, ao criar um objeto.
+# construtor é o __init__
 
+# Os atributos podem ser encapsulados como:
+# public - atributo público, pode ser acessado por todas as classes (sem underscore)
+# private - atributo encapsulado e contido em uma única classe (__ antes do nome) - não será possível chamar a porpriedade diretamente.
+# protect - envia um 'sinal de captura' para o acesso por outra classe ser permitido (_ antes do nome)
+
+# @property - decorador que remove parênteses do chamado e retorna o valor que foi dado para o atributo
+# '__atributo' no nosso init irá chamar nosso método getter para atualizar os atributos
+# @atributo.setter - recebe o valor do getter no atributo da classe.
+
+# Caminho do atributo: atribuição (criação do obj) - construtor - getter - setter
+# A setter recebe o valor que foi atribuido na criação do objeto e iguala ele ao atributo.
 
 class Livro:
-    def __init__(self, nome, autores, genero):
+    def __init__(self, nome, autores, genero, id):
+        self.__id = id # Método getter
         self.__nome = nome
         self.__autores = autores
         self.__genero = genero
-
-    # Decorador que permite o acesso do método nome como propriedade, para acessar nome
-    # (indiretamente) no código principal.
-    @property  
+        
+    
+    # Declaração do getter
+    @property
+    def id(self):
+        return self.__id
+    
+    @id.setter
+    def id(self, id):
+        self.__id = id # id é a função getter, logo ele pega o valor da getter e atribui no parâmetro correto - ele 'seta'.
+    
+    
+    @property
     def nome(self):
         return self.__nome
     
-    # Setter permite que o valor recebido de nome (no código principal) seja atrobuído
-    # Na instância da classe.
     @nome.setter
-    def nome(self, nome):
+    def id(self, nome):
         self.__nome = nome
+        
     
-
-    @property  
+    @property
     def autores(self):
         return self.__autores
     
     @autores.setter
-    def nome(self, autores):
-        self.__autor = autores
+    def autores(self, autores):
+        self.__autores = autores
+        
     
-
-    @property  
+    @property
     def genero(self):
         return self.__genero
     
-    @genero.setter
-    def nome(self, genero):
+    @id.setter
+    def genero(self, genero):
         self.__genero = genero
+        
+    
+    def getDict(self):
+        return {self.__id : {'nome' : self.__nome, 'autores' : self.__autores, 'genero' : self.__genero}}
+    
 
 
-
+# OBS: 'id' SEMPRE irá se referir ao ID do livro no JSON, NUNCA à posição dele na lista (um index abaixo).
 class Biblioteca:
-    def pegarJSON(): 
+    def __init__(self):
+        self.__data = self.pegarJSON()
+        
+        
+    @property
+    def data(self):
+        return self.__data
+    
+    
+    def pegarJSON(self): 
         try:
-            livros = []
-            with open('orientado-a-objeto/db.json', 'r', encoding='utf-8') as file:
-                data = (json.load(file))
+            with open(f'{'orientado-a-objeto'}/db.json', 'r', encoding='utf-8') as file:
+                self.__data = json.load(file)
+                return self.__data
 
+        except json.decoder.JSONDecodeError:
+            return []
+
+
+    def adicionarAoJSON(self, data):
+        with open(f'{'orientado-a-objeto'}/db.json', 'w', encoding='utf-8') as file:
+            json.dump(data, file, ensure_ascii=False, indent=4)
+            
+    
+    def pegarID(self):
+        data = self.pegarJSON()
+        id = 0
+        
+        if data != []:
             for dicts in data:
                 for key in dicts:
-                    livros.append(key)
+                    id = int(key)
+                
+            return (str(id + 1))
+        else:
+            return 1
+            
 
-            return data, livros
+    def cadastrarLivro(self, nome, autores, genero, id=None, mode='append'):
+        if id == None:
+            id = self.pegarID()
         
-        except json.decoder.JSONDecodeError:
+        novoLivro = Livro(nome, autores, genero, id)
+        
+        if mode == 'replace':
+            self.__data[id] = novoLivro.getDict()
+            self.adicionarAoJSON(self.__data)
+        else:
+            self.__data.append(novoLivro.getDict())
+            self.adicionarAoJSON(self.__data)
+    
+    
+    def visualizarLivros(self, returnID=False):
+        data = self.pegarJSON()
+        
+        if data != []:
+            for dicts in data:
+                for id in dicts:
+                    print(f'Livro {id}: ')
+                    print(f'Nome: {dicts[id]['nome']}')
+                    print(f'Autores: {dicts[id]['autores']}')
+                    print(f'Gênero: {dicts[id]['genero']} \n')
+        else:
+            print('Não há nenhum livro cadastrado.')
+            
+        if returnID == True:
+            return int(id)
+        
+        
+    def IDParaInfo(self, id, autores=False, genero=False):
+        data = self.pegarJSON()
+        
+        for dicts in data:
+            if str(id) in dicts: # Preciso checar se o id fornecido é key, para poder manipular
+                                # caso contrário, teria que manipular com um intervalo fixo - o que gera conflito
+                                # com IDs que não são subsequentes.
+                if autores == True:
+                    return dicts[str(id)]['autores']
+                elif genero == True:
+                    return dicts[str(id)]['genero']
+                else:
+                    return dicts[str(id)]['nome']
 
-            return [], []
+    
+    def editarLivros(self, id, modified, toEdit):
+        data = self.pegarJSON()
+        
+        try:
+            for dicts in data:
+                if str(id) in dicts:
+                    if toEdit == 'nome':
+                        dicts[str(id)]['nome'] = modified
+                    elif toEdit == 'autores':
+                        dicts[str(id)]['autores'] = modified
+                    elif toEdit == 'genero':
+                        dicts[str(id)]['genero'] = modified
 
+            self.adicionarAoJSON(data)
+        except Exception as e:
+            print(e)
+            
+    
+    def deletarLivro(self, id):
+        data = self.pegarJSON()
+        
+        try:
+            for index, dicts in enumerate(data):
+                if str(id) in dicts:
+                    data.pop(index)
+                        
+            self.adicionarAoJSON(data)
+        except Exception as e:
+            print(e)
+            
 
-    def adicionarAoJSON(data):
-        with open('orientado-a-objeto/db.json', 'w', encoding='utf-8') as file:
-            json.dump(data, file, ensure_ascii=False, indent=4)
-
-
-    def cadastrar(nome, autores, genero):
-        novoLivro = Livro(nome, autores, genero)
-        system('cls')    
-        print(f'Novo livro cadastrado: {novoLivro}')
-        sleep(1)
-        system('cls')
-
-
-    def editar():
-        print('Editor de livros cadastrados''\n')
-
-
-    def remover():
-        while True:
-            print('Deleção de livro cadastrado''\n')
--- orientado-a-objeto/lib.py
+++ orientado-a-objeto/lib.py
@@ -1,61 +1,209 @@
 from time import sleep
 from os import system
 from classes import Biblioteca
-import json
 
-# Função básica, que utiliza os comandos de CLI para limpar a tela.
-# Utilizado para gegar erro genérico em muitos lugares do código.
-def continuar():
+def continuar(mensagem):
     system('cls')
-    print('Opção inválida. Tente novamente.')
-    sleep(0.5)
+    print(mensagem)
+    sleep(1)
     system('cls')
+    
 
-
-def pegarSTR(inputValue):
-    string = (input(inputValue).title()).strip()
-    return string
+def pegarSTR(mensagem, erro):
+    while True:
+        string = (input(mensagem).title()).strip()
+        
+        if string == '':
+            continuar(erro)
+        else:
+            return string
 
 
 def pegarAutores():
     autores = []
-    c = 1 # Contador
+    c = 1
+    
     while True:
         system('cls')
-        autor = (input(f'Digite o nome do {c}° autor do livro (enter para parar): ').title()).strip()
-        # Se for diferente de enter, irá adicionar o valor à lista e seguir para a próxima iteração.
+        autor = (input(f'Insira o {c}° autor do livro (enter para parar): ').title()).strip()
+        
         if autor != '':
             autores.append(autor)
             c += 1
             continue
-        # Se autor == enter e a lista estiver com algom valor, finaliza a execução e retorna.
-        elif (autor == '') and (autores != []):
-            system('cls')
-            print('Todos os autores foram cadastrados.')
-            return autores
-        # Se autor == enter e a lista estiver vazia, apenas envia a mensagem padrão de erro.
-        else:
-            continuar()
+        elif (autor == '') and (c == 1):
+            continuar('Valor inválido. Insira pelo menos um autor.')
             continue
+        else:
+            continuar('Todos os autores cadastrados.')
+            return autores      
 
 
+def cadastrar(id=None, staticID=False, mode='append'):
+    lib = Biblioteca()
+    
+    system('cls')
+    print('--- Menu de Cadastro de Livro --- \n')
+    nome = pegarSTR('Insira o nome do livro: ', 'Nome inválido. Tente novamente.')
+    autores = pegarAutores()
+    genero = pegarSTR('Insira o gênero do livro: ', 'Valor inválido. Tente novamente.')
+    
+    if staticID == True:
+        lib.cadastrarLivro(nome, autores, genero, id, mode='replace')
+    else:
+        lib.cadastrarLivro(nome, autores, genero)
+    
+    print(f'Nome: {nome}')
+    print(f'Autores: {autores}') 
+    print(f'Gênero: {genero}')
+    print('Livro cadastrado com sucesso!')
+    sleep(1)
+            
+            
+def visualizar(returnID=False):
+    lib = Biblioteca()
+    if returnID == False:
+        lib.visualizarLivros()
+    else:
+        id = lib.visualizarLivros(returnID=True)
+        return id
+    
+    
+def editar():
+    lib = Biblioteca()
+    
+    while True:
+        system('cls')
+        print('--- Menu de Edição de Livro --- \n')
+        id = visualizar(returnID=True)
+        print(f'{id + 1}: Retornar ao menu principal \n')
+        opt = input('Escolha o que deseja fazer: ')
+        
+        try:
+            opt = (int(opt))
+            if (opt < 0) or (opt > (id + 1)):
+                raise ValueError  
+        except ValueError:
+            continuar('Valor inválido. Por favor, tente novamente.')
+            continue
+            
+        if opt < (id + 1):
+            while True:
+                system('cls')
+                print(f'Editando livro: {lib.IDParaInfo(opt)}')
+                print('1 - Editar nome do livro')
+                print('2 - Editar autores do livro')
+                print('3 - Editar gênero do livro')
+                print('4 - Editar tudo (recadastrar livro)')
+                print('5 - Retornar ao Menu Principal \n')
+
+                opt2 = input('Escolha o que deseja fazer: ').strip()
+
+                match opt2:
+                    case '1':
+                        system('cls')
+                        nome = pegarSTR('Insira o nome do livro: ', 'Nome inválido. Por favor, tente novamente.')
+                        lib.editarLivros(opt, nome, 'nome')
+                        continuar('Nome do livro alterado com sucesso!')
+                        break              
+                    case '2':
+                        system('cls')
+                        autores = pegarAutores()
+                        lib.editarLivros(opt, autores, 'autores')
+                        break
+                    case '3':
+                        system('cls')
+                        genero = pegarSTR('Insira o gênero do livro: ', 'Valor inválido. Por favor, tente novamente.')
+                        lib.editarLivros(opt, genero, 'genero')
+                        continuar('Gênero do livro alterado com sucesso!')
+                        break
+                    case '4':
+                        cadastrar(opt, staticID=True, mode='replace')
+                        break
+                    case '5':
+                        continuar('Retornando ao menu principal.')
+                        break
+                    case _:
+                        continuar('Valor inválido. Por favor, tente novamente.')
+                        continue
+        elif opt == (id + 1):
+            continuar('Retornando ao menu principal.')
+            break
+        else:
+            continuar('Valor inválido. Por favor, tente novamente.')
+            continue
+        
+        break
+    
+
+def remover():
+    lib = Biblioteca()
+    
+    while True:
+        system('cls')
+        print('--- Menu de Deleção de Livro --- \n')
+        id = visualizar(returnID=True)
+        print(f'{id + 1}: Retornar ao menu principal \n')
+        
+        opt = input('Escolha o livro que deseja deletar: ')
+        
+        try:
+            opt = (int(opt))
+            if (opt < 0) or (opt > (id + 1)):
+                raise ValueError  
+        except ValueError:
+            continuar('Valor inválido. Por favor, tente novamente.')
+            continue
+            
+        if opt < (id + 1):
+            while True:
+                system('cls')
+                print(f'{lib.IDParaInfo(opt)}')
+                opt2 = (input('Tem certeza que deseja deletar o livro? (s/n): ').lower()).strip()
+
+                match opt2:
+                    case 's':
+                        print(f'Deletando o livro: {lib.IDParaInfo(opt)}...')
+                        lib.deletarLivro(opt)
+                        continuar('Livro deletado com sucesso!')
+                        break
+                        
+                    case 'n':
+                        continuar('Operação cancelada. Retornando ao menu principal.')
+                        break
+                        
+                    case _:
+                        continuar('Valor inválido. Por favor, tente novamente.')
+                        continue
+                        
+
+        elif opt == (id + 1):
+            continuar('Retornando ao menu principal.')
+            break
+        else:
+            continuar('Valor inválido. Por favor, tente novamente.')
+            continue
+        
+        break
+    
+        
 def main():
     while True:
-        data, livros = Biblioteca.pegarJSON()
-        system('cls')
+        lib = Biblioteca()
 
-        if livros == []:
+        system('cls')
+        if lib.data == []:
             opt = (input('A lista de livros está vazia. Deseja cadastrar um livro? (s/n): ').lower()).strip()
             if opt == 's':
                 system('cls')
-                Biblioteca.cadastrar()
+                cadastrar()
                 continue
             elif opt == 'n':
                 system('cls')
                 print('Adeus, amigo.')
                 break
             else:
-                continuar()
+                continuar('Valor inválido. Por favor, tente novamente.')
                 continue
         else:
             system('cls')
@@ -70,30 +218,28 @@ def main():
             match opt:
                 case '1':
                     system('cls')
-
-                    sleep(5)
+                    lib.visualizarLivros()
+                    sleep(8)
                     continue
-
                 case '2':
                     system('cls')
-                    
+                    cadastrar()
                     continue
                 case '3':
                     system('cls')
-                    
+                    editar()
                     continue
                 case '4':
                     system('cls')
-                    
+                    remover()
                     continue
                 case '5':
                     system('cls')
                     print('Adeus, amigo.')
                     break
                 case _:
-                    continuar()
+                    continuar('Valor inválido. Por favor, tente novamente.')
                     continue
 
 
 main()
-
